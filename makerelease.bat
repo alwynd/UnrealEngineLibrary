98535570ba41f4597325e2e17cf719ef1c479476
alwynd <alwynd@goofishgamestudios.com>
2023-12-26 10:09:20 +0200

    release refinements

--- makerelease.bat
+++ makerelease.bat
@@ -10,29 +10,34 @@ IF NOT EXIST %ReleaseDir% (
 ) ELSE (
     DEL /Q /F %ReleaseDir%\*.*
 )
-
 IF ERRORLEVEL 1 (
-    echo Failed to clean the Release folder.
+    echo "Failed to clean the Release folder: %ReleaseDir%"
     pause
     exit /b 99
 )
 
 :: Copy the necessary files.
 XCOPY /Y /S "%SourceDir%\*" %ReleaseDir%\
-
 IF ERRORLEVEL 1 (
-    echo Failed to copy files to the Release folder.
+    echo "Failed to copy files(1) from %SourceDir% to the Release folder: %ReleaseDir%"
     pause
     exit /b 98
 )
 
+XCOPY /Y /S "ReleaseScripts\*" %ReleaseDir%\
+IF ERRORLEVEL 1 (
+    echo "Failed to copy files(2) from ReleaseScripts to the Release folder: %ReleaseDir%"
+    pause
+    exit /b 95
+)
+
+
 :: Run a PowerShell command to Zip it UP.
 Powershell.exe -Command "& { Add-Type -assembly 'system.io.compression.filesystem'; [io.compression.zipfile]::CreateFromDirectory('%CD%\%ReleaseDir%', '%zipFile%'); }"
-
 IF ERRORLEVEL 1 (
-    echo Failed to create a zip file.
+    echo "Failed to create the zip file: %zipFile%"
     pause
-    exit /b 98
+    exit /b 90
 )
 
 :: Delete copied files
