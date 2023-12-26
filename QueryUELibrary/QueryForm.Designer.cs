5ac5e4e468f5a5d88c110275021047f369f6ca66
alwynd <alwynd@goofishgamestudios.com>
2023-12-26 09:37:02 +0200

    externalize html help, refactor and promote buttons, labels to public

--- QueryUELibrary/QueryForm.Designer.cs
+++ QueryUELibrary/QueryForm.Designer.cs
@@ -5,7 +5,7 @@ namespace QueryUELibrary
     partial class QueryForm
     {
 
-public readonly static string HelpSnippet = "<!DOCTYPE html>\n<html>\n<head>\n<style>\nbody {\nbackground-color: black;\ncolor: #D3D3D3;\nfont-family: Arial, sans-serif;\n}\nh1 {\nfont-weight: bold;\n}\n.code-snippet {\nbackground-color: #2A2A2A;\nborder: solid thin white;\npadding: 20px;\nwhite-space: pre;\ncolor: #D3D3D3;\n}\n</style>\n</head>\n<body>\n<h1>Unreal Engine Library JSON Structure</h1>\n<div class=\"code-snippet\">\n\"/Game/Characters/Mannequins/Meshes/SK_Mannequin.SK_Mannequin\":<br/>\n{\n&nbsp;&nbsp;&nbsp;&nbsp;\"AssetPath\": \"/Game/Characters/Mannequins/Meshes/SK_Mannequin.SK_Mannequin\",\n&nbsp;&nbsp;&nbsp;&nbsp;\"SizeOnDisk\": 160872,\n&nbsp;&nbsp;&nbsp;&nbsp;\"AssetType\": \"/Script/Engine.Skeleton\"\n}\n</div>\n<p style='color:white;'>Each JSON file in the UELibrary consists of arrays using this structure.<br/>To query the JSON, use PowerShell Where-Object (examples are included in the main form).<br/>Asset types are derived from Unreal Engine, so you can use PowerShell Where-Object to find unique asset types.<br/><br/>The PowerShell Where-Object query will run against all files in the UELibrary.<br/>Use smart, case insensitive searches when looking for specific asset types, such as 'assettype contains xyz'.<br/>You can also include logical operators like '-and' and '-or' in your search queries.<br/><br/>To refine searches further, combine these techniques with asset names, or even sizes (where the size is the number of bytes on disk).</p>\n<p style='color:white;'>The following assets (and their thumbnails) were extracted and classified from Unreal Engine Projects:<ul style='color:white;'><li>Textures</li>\n<li>Materials (includes instances)</li>\n<li>Static/Skeletal Meshes</li>\n<li>Animations</li>\n<li>Blueprints</li>\n<li>Any other Unreal Objects included may not have thumbnails</li>\n</ul></p>\n<p style='color:white;'><b>Query examples:</b><br/>\n$_.AssetPath -match 'spruce' -and $_.AssetType -match 'staticmesh'\n- look for all assets, containing the text 'spruce', where the asset is a static mesh.<br/><br/>\n$_.SizeOnDisk -gt 104857600\n- Look for all assets, where the size on disk, is greater than 100MB<br/><br/>\n$_.AssetPath -match 'combat.*sword' -and $_.AssetType -match 'animation'\n- Look for all assets, where the name regex matches combat and sword, where the asset type is an animation.<br/></p>\n</body>\n</html>";                
+
         /// <summary>
         ///  Required designer variable.
         /// </summary>
@@ -26,7 +26,36 @@ public readonly static string HelpSnippet = "<!DOCTYPE html>\n<html>\n<head>\n<s
         /// </summary>
         public readonly TextBox JQQuery = new TextBox();
 
+        /// <summary>
+        /// The examples button.
+        /// </summary>
+        public readonly Button ExamplesButton = new Button();
 
+        /// <summary>
+        /// The label for Script path.
+        /// </summary>
+        public readonly Label ScriptPathLabel = new Label();
+
+        /// <summary>
+        /// The query input label. 
+        /// </summary>
+        public readonly Label QueryInputLabel = new Label();
+
+        /// <summary>
+        /// The status bar windows nonsnese.
+        /// </summary>
+        public readonly StatusStrip StatusBar = new StatusStrip();
+        
+        /// <summary>
+        /// The status bar windows nonsnese.
+        /// </summary>
+        public readonly ToolStripStatusLabel StatusLabel = new ToolStripStatusLabel();
+        
+        /// <summary>
+        /// The help button.
+        /// </summary>
+        public readonly Button HTMLHelpButton = new Button();
+        
         /// <summary>
         ///  Clean up any resources being used.
         /// </summary>
@@ -58,45 +87,28 @@ public readonly static string HelpSnippet = "<!DOCTYPE html>\n<html>\n<head>\n<s
             Name = "QueryForm";
             Text = "Query UELibrary";
 
-            StatusStrip statusBar = new StatusStrip();
-            ToolStripStatusLabel statusLabel = new ToolStripStatusLabel();
-            statusLabel.Text = "Version: 1.0, alwyn.j.dippenaar@gmail.com";
-            statusBar.Items.Add(statusLabel);            
-            this.Controls.Add(statusBar);            
+            StatusBar.Items.Add(StatusLabel);            
+            this.Controls.Add(StatusBar);            
 
             // Creating new "hyperlinked" label and text box for "Path to Powershell script"
-            Label lblCygwinPath = new Label();
-            lblCygwinPath.Text = "Path to Powershell Script:";
-            lblCygwinPath.Font = new Font(lblCygwinPath.Font, FontStyle.Bold | FontStyle.Underline);
-            lblCygwinPath.ForeColor = Color.Blue;
-            lblCygwinPath.SetBounds(10, 35, 800, 20);
-            lblCygwinPath.Cursor = Cursors.Hand;
-
-            // Add event handler for click event
-            lblCygwinPath.Click += (sender, args) => 
-            {
-                Process.Start(new ProcessStartInfo("cmd", $"/c start https://learn.microsoft.com/en-us/powershell/") { CreateNoWindow = true });
-            };
+            ScriptPathLabel.Text = "Path to Powershell Script:";
+            ScriptPathLabel.Font = new Font(ScriptPathLabel.Font, FontStyle.Bold | FontStyle.Underline);
+            ScriptPathLabel.ForeColor = Color.Blue;
+            ScriptPathLabel.SetBounds(10, 35, 800, 20);
+            ScriptPathLabel.Cursor = Cursors.Hand;
             
             ScriptPath.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             ScriptPath.SetBounds(10, 55, ClientSize.Width - 20, 20);
             ScriptPath.Text = "query.ps1";
-            this.Controls.Add(lblCygwinPath);
+            this.Controls.Add(ScriptPathLabel);
             this.Controls.Add(ScriptPath);            
 
             // Creating new label and text area for the input area
-            Label lblTextArea = new Label();
-            lblTextArea.Text = "PS Where-Object Query: (replaces {QUERY_JQ} in the script)";
-            lblTextArea.Font = new Font(lblCygwinPath.Font, FontStyle.Bold | FontStyle.Underline);
-            lblTextArea.ForeColor = Color.Blue;
-            lblTextArea.Cursor = Cursors.Hand;
-            lblTextArea.SetBounds(10, 85, 150, 20);
-
-            // Add event handler for click event
-            lblTextArea.Click += (sender, args) => 
-            {
-                Process.Start(new ProcessStartInfo("cmd", $"/c start https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/where-object") { CreateNoWindow = true });
-            };
+            QueryInputLabel.Text = "PS Where-Object Query: (replaces {QUERY_JQ} in the script)";
+            QueryInputLabel.Font = new Font(ScriptPathLabel.Font, FontStyle.Bold | FontStyle.Underline);
+            QueryInputLabel.ForeColor = Color.Blue;
+            QueryInputLabel.Cursor = Cursors.Hand;
+            QueryInputLabel.SetBounds(10, 85, 150, 20);
             
             JQQuery.Anchor = AnchorStyles.Top | AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             JQQuery.Multiline = true;
@@ -104,7 +116,7 @@ public readonly static string HelpSnippet = "<!DOCTYPE html>\n<html>\n<head>\n<s
             JQQuery.SetBounds(10, 105, ClientSize.Width - 20, 100);
 
             // Add new controls to the form
-            this.Controls.Add(lblTextArea);
+            this.Controls.Add(QueryInputLabel);
             this.Controls.Add(JQQuery);            
             
             // New y-coordinate for the button
@@ -119,46 +131,15 @@ public readonly static string HelpSnippet = "<!DOCTYPE html>\n<html>\n<head>\n<s
             this.Controls.Add(QueryButton);                        
 
             // Create the Button
-            Button helpButton = new Button();
-            helpButton.Text = "Json";
-            helpButton.Font = new Font(QueryButton.Font, FontStyle.Bold);
-            helpButton.SetBounds(QueryButton.Right + 10, QueryButton.Top, 100, 45);
-            helpButton.Click += (sender, args) =>
-            {
-                // help form, with centered label.
-                Form myForm = new Form
-                {
-                    Text = "Json Structure",
-                    Size = new Size(Screen.PrimaryScreen.Bounds.Width / 3, Screen.PrimaryScreen.Bounds.Height / 3),
-                    StartPosition = FormStartPosition.CenterScreen
-                };
+            HTMLHelpButton.Text = "Json";
+            HTMLHelpButton.Font = new Font(QueryButton.Font, FontStyle.Bold);
+            HTMLHelpButton.SetBounds(QueryButton.Right + 10, QueryButton.Top, 100, 45);
+            this.Controls.Add(HTMLHelpButton);                        
 
-                // Create a new WebBrowser control
-                WebBrowser webBrowser = new WebBrowser
-                {
-                    DocumentText = HelpSnippet,
-                    AllowNavigation = false,
-                    Dock = DockStyle.Fill, //Dock the control
-                    IsWebBrowserContextMenuEnabled = false,
-                };
-   
-                myForm.Controls.Add(webBrowser);
-                myForm.ShowDialog();                
-            };
-            this.Controls.Add(helpButton);                        
-
-            Button examples = new Button();
-            examples.Text = "Examples";
-            examples.Font = new Font(QueryButton.Font, FontStyle.Bold);
-            examples.SetBounds(helpButton.Right + 10, helpButton.Top, 100, 45);
-            examples.Click += (sender, args) =>
-            {
-                JQQuery.Text = "Remember, ONLY 1 Object-Where query is allowed at a time, the below is just examples of such queries, submitting this \"as is\" will result in an error\r\n" +
-                               "$_.AssetPath -match \"spruce\" -and $_.AssetType -match \"staticmesh\"\r\n\t # - look for all assets, containing the text \"spruce\", where the asset is a static mesh.\r\n\r\n" +
-                               "$_.SizeOnDisk -gt 104857600\r\n\t #- Look for all assets, where the size on disk, is greater than 100MB.\r\n\r\n" +
-                               "$_.AssetPath -match \"combat.*sword\" -and $_.AssetType -match \"animation\"\r\n\t #- Look for all assets, where the name regex matches combat and sword, where the asset type is an animation.";
-            };
-            this.Controls.Add(examples);                        
+            ExamplesButton.Text = "Examples";
+            ExamplesButton.Font = new Font(QueryButton.Font, FontStyle.Bold);
+            ExamplesButton.SetBounds(HTMLHelpButton.Right + 10, HTMLHelpButton.Top, 100, 45);
+            this.Controls.Add(ExamplesButton);                        
             
             
             ResumeLayout(false);
--- QueryUELibrary/QueryForm.cs
+++ QueryUELibrary/QueryForm.cs
@@ -1,17 +1,71 @@
+using System.Diagnostics;
+
 namespace QueryUELibrary
 {
+    /// <summary>
+    /// The query form.
+    /// </summary>
     public partial class QueryForm : Form
     {
+        private static readonly string Version = "Version: 1.0, alwyn.j.dippenaar@gmail.com";
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public QueryForm()
         {
             InitializeComponent();
+            StatusLabel.Text = Version;
             
-            // Add a Click Event Handler
+            // Do the Query.
             QueryButton.Click += (sender, args) =>
             {
-                // Code to run when the button is clicked
                 MessageBox.Show("The 'Query' button has been clicked.");
             };
+            
+            // Goto URL.
+            ScriptPathLabel.Click += (sender, args) => 
+            {
+                Process.Start(new ProcessStartInfo("cmd", $"/c start https://learn.microsoft.com/en-us/powershell/") { CreateNoWindow = true });
+            };
+            QueryInputLabel.Click += (sender, args) => 
+            {
+                Process.Start(new ProcessStartInfo("cmd", $"/c start https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/where-object") { CreateNoWindow = true });
+            };
+
+            // Update the query text with some examples.
+            ExamplesButton.Click += (sender, args) =>
+            {
+                JQQuery.Text = "Remember, ONLY 1 Object-Where query is allowed at a time, the below is just examples of such queries, submitting this \"as is\" will result in an error\r\n" +
+                               "$_.AssetPath -match \"spruce\" -and $_.AssetType -match \"staticmesh\"\r\n\t # - look for all assets, containing the text \"spruce\", where the asset is a static mesh.\r\n\r\n" +
+                               "$_.SizeOnDisk -gt 104857600\r\n\t #- Look for all assets, where the size on disk, is greater than 100MB.\r\n\r\n" +
+                               "$_.AssetPath -match \"combat.*sword\" -and $_.AssetType -match \"animation\"\r\n\t #- Look for all assets, where the name regex matches combat and sword, where the asset type is an animation.";
+            };
+
+            // html help.
+            HTMLHelpButton.Click += (sender, args) =>
+            {
+                // help form, with centered label.
+                Form myForm = new Form
+                {
+                    Text = "Json Structure",
+                    Size = new Size(Screen.PrimaryScreen.Bounds.Width / 3, Screen.PrimaryScreen.Bounds.Height / 3),
+                    StartPosition = FormStartPosition.CenterScreen
+                };
+
+                // Create a new WebBrowser control
+                WebBrowser webBrowser = new WebBrowser
+                {
+                    DocumentText = File.ReadAllText("help.html"), // if it does not exists, then bomb
+                    AllowNavigation = false,
+                    Dock = DockStyle.Fill, //Dock the control
+                    IsWebBrowserContextMenuEnabled = false,
+                };
+   
+                myForm.Controls.Add(webBrowser);
+                myForm.ShowDialog();                
+            };
+            
             
         }
     }
